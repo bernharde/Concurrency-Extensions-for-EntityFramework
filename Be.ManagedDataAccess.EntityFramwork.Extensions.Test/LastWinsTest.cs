81a671167834ebee5ab83f5f4aa13435b4afec7a
bernharde <bernhardelbl@hotmail.com>
2019-03-13 19:44:08 +0100

    .

---
+++ Be.ManagedDataAccess.EntityFramwork.Extensions.Test/LastWinsTest.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Be.ManagedDataAccess.EntityFramework.Test
+{
+    [TestClass]
+    public class LastWins
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void DublicateKey()
+        {
+            var id = Guid.NewGuid();
+
+            using (var cx = new OracleDbContext())
+            {
+                var lw = new LastWinsEntity();
+                lw.Id = id;
+                lw.Name = "v1";
+                lw.Created = lw.Updated = DateTime.Now;
+                cx.LastWins.Add(lw);
+                cx.SaveChanges();
+            }
+            
+            using (var cx = new OracleDbContext())
+            {
+                try
+                {
+                    var lw = new LastWinsEntity();
+                    lw.Id = id;
+                    lw.Name = "v2";
+                    lw.Created = lw.Updated = DateTime.Now;
+                    cx.LastWins.Add(lw);
+                    cx.SaveChanges();
+                }
+                catch(DbUpdateException dex)
+                {
+                    var oex = dex.InnerException?.InnerException as OracleException;
+                    if(oex != null)
+                    {
+                        if(oex.Number == 1) // 1 = dublicate key error number
+                        {
+                            throw new Exception("dublicate key", dex);
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void DublicateKey_Context()
+        {
+            var id = Guid.NewGuid();
+            var lwc = CreateOptiContext();
+
+            lwc.SelectFunc = (cx) =>
+            {
+                var i = cx.LastWins.Find(id);
+                return i;
+            };
+
+            lwc.AddAction = (cx) =>
+            {
+                var lw = new LastWinsEntity();
+                lw.Id = id;
+                lw.Name = "v1";
+                lw.Created = lw.Updated = DateTime.Now;
+                cx.LastWins.Add(lw);
+                cx.SaveChanges();
+            };
+            lwc.UpdateAction = (cx, lw) =>
+            {
+                lw.Name = "v2";
+                lw.Created = lw.Updated = DateTime.Now;
+                cx.SaveChanges();
+            };
+
+            // begin select
+            var cx2 = lwc.ContextFactory();
+            var entity = lwc.SelectFunc(cx2);
+
+            // add in the meantime
+            lwc.Execute(); // first add
+
+            if(entity == null) // entity is always null
+                lwc.TryAdd(cx2); // simulate add with dublicate key
+        }
+
+        [TestMethod]
+        public void DublicateKeyHandling_AddUpdate()
+        {
+            var id = Guid.NewGuid();
+
+            var lwc = CreateOptiContext();
+            lwc.SelectFunc = (cx) =>
+                {
+                    return cx.LastWins.FirstOrDefault(e => e.Id == id);
+                };
+            lwc.AddAction = (cx) =>
+                {
+                    var lw = new LastWinsEntity();
+                    lw.Id = id;
+                    lw.Name = "v2";
+                    lw.Created = lw.Updated = DateTime.Now;
+                    cx.LastWins.Add(lw);
+                    cx.SaveChanges();
+                };
+            lwc.UpdateAction = (cx, lw) =>
+                {
+                    lw.Name = "v2";
+                    lw.Updated = DateTime.Now;
+                    cx.SaveChanges();
+                };
+
+            lwc.Execute(); // add
+            lwc.Execute(); // update
+        }
+
+        public OptiContext<OracleDbContext, LastWinsEntity> CreateOptiContext()
+        {
+            var result = new OptiContext<OracleDbContext, LastWinsEntity>(() => new OracleDbContext());
+            return result;
+        }
+
+        [TestMethod]
+        public void DublicateKeyHandling_AddDeleteAdd()
+        {
+            var id = Guid.NewGuid();
+            var lwc = CreateOptiContext();
+
+            lwc.SelectFunc = (cx) =>
+            {
+                var i = cx.LastWins.Find(id);
+                return i;
+            };
+
+            lwc.AddAction = (cx) =>
+            {
+                var lw = new LastWinsEntity();
+                lw.Id = id;
+                lw.Name = "v1";
+                lw.Created = lw.Updated = DateTime.Now;
+                cx.LastWins.Add(lw);
+                cx.SaveChanges();
+            };
+            lwc.UpdateAction = (cx, lw) =>
+            {
+                lw.Name = "v2";
+                lw.Created = lw.Updated = DateTime.Now;
+                cx.SaveChanges();
+            };
+
+            lwc.Execute(); // add
+
+            Delete(id); // delete in the meantime
+
+            lwc.Execute(); // add 2
+        }
+
+        [TestMethod]
+        public void DublicateKeyHandling_AddSelectDeleteUpdate()
+        {
+            var id = Guid.NewGuid();
+            var lwc = CreateOptiContext();
+
+            lwc.SelectFunc = (cx) =>
+            {
+                var i = cx.LastWins.Find(id);
+                return i;
+            };
+
+            lwc.AddAction = (cx) =>
+            {
+                var lw = new LastWinsEntity();
+                lw.Id = id;
+                lw.Name = "v1";
+                lw.Created = lw.Updated = DateTime.Now;
+                cx.LastWins.Add(lw);
+                cx.SaveChanges();
+            };
+            lwc.UpdateAction = (cx, lw) =>
+            {
+                lw.Name = "v2";
+                lw.Created = lw.Updated = DateTime.Now;
+                cx.SaveChanges();
+            };
+            lwc.Execute();
+
+            // simulate Execute part 1 -> create context and select entity
+            var cx2 = lwc.ContextFactory();
+            var entity = lwc.SelectFunc(cx2);
+
+            // delete in the meantime
+            Delete(id); 
+
+            // simulate Execute part 2 -> TryUpdate the existing entity. Its not here, so AddAction is called
+            lwc.TryUpdate(cx2, entity);
+        }
+
+        private void Delete(Guid id)
+        {
+            Task.Run(() =>
+            {
+                using (var cx = new OracleDbContext())
+                {
+                    var lw = cx.LastWins.FirstOrDefault(e => e.Id == id);
+                    cx.LastWins.Remove(lw);
+                    cx.SaveChanges();
+                }
+            }).Wait();
+        }
+    }
+}
---
+++ Be.ManagedDataAccess.EntityFramwork.Extensions.Test/OptiEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Be.ManagedDataAccess.EntityFramework.Test
+{
+    [Table("XXCO")]
+    public class OptiEntity
+    {
+        [Key]
+        [Column("ID")]
+        public Guid Id { get; set; }
+
+        [Column("VALUE")]
+        public int Value { get; set; }
+
+        [Column("CREATED")]
+        public DateTime Created { get; set; }
+
+        [Column("UPDATED")]
+        public DateTime Updated { get; set; }
+
+        [Column("ROWVERSION")]
+        [ConcurrencyCheck]
+        public Guid RowVersion { get; set; }
+    }
+}
