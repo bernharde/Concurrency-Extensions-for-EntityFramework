521d848b3ff1293d15016b97140bb078ca23bfb9
bernharde <bernhardelbl@hotmail.com>
2019-03-13 21:06:09 +0100

    .

--- Be.EntityFramework.SqlServer.Extensions/Properties/AssemblyInfo.cs
+++ Be.EntityFramework.SqlServer.Extensions/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Be.EntityFramework.SqlServer.Extensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adds extension methods to deal with concurrency using EntityFramework.SqlServer. Contains AddOrUpdate like behaviors and more.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Be")]
 [assembly: AssemblyProduct("Be.EntityFramework.SqlServer.Extensions")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©Be 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
