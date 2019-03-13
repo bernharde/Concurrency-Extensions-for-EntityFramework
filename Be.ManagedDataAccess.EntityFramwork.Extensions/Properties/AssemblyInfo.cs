21b3b1caa183d120405dfdcd2d3a16a91406bfb0
bernharde <bernhardelbl@hotmail.com>
2019-03-13 14:50:21 +0100

    .

--- Be.ManagedDataAccess.EntityFramwork.Extensions/Properties/AssemblyInfo.cs
+++ Be.ManagedDataAccess.EntityFramwork.Extensions/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Be.ManagedDataAccess.EntityFramwork.Extensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adds extension methods to deal with concurrency using Oracle.ManagedDataAccess.EntityFramework. Contains AddOrUpdate like behaviors and more.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("bemento - Bernhard Elbl")]
+[assembly: AssemblyCompany("Be")]
 [assembly: AssemblyProduct("Be.ManagedDataAccess.EntityFramwork.Extensions")]
 [assembly: AssemblyCopyright("Copyright ©Be 2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
