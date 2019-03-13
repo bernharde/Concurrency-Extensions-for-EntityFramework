b1c3bcfa517596810356631b94a35b7ae6790c20
Bernhard Elbl <bernhardelbl@hotmail.com>
2019-03-13 18:05:16 +0100

    .

---
+++ Be.EntityFramworkCore.SqlServer.Extensions.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Be.EntityFramworkCore.SqlServer.Extensions.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Be.EntityFramworkCore.SqlServer.Extensions/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Be.EntityFramworkCore.SqlServer.Extensions
+{
+    public class Class1
+    {
+    }
+}
